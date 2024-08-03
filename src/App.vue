1d693057c8480ab2ae0541859bf49f52971921e4
“Sepn1ne” <369969927@qq.com>
2024-08-03 14:18:34 +0800

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 重定位到  / -->
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  mounted() {
+    console.log('根组件挂载')
+    const myToken = localStorage.getItem('token')
+    if (myToken == null) {
+      console.log('没有token')
+      localStorage.clear()
+    } else {
+      //在根组件挂载的时候，向后端发起请求来判断token是否过期
+      //如果过期，则清除token，使得用户需要重新登录
+      //否则用户无需登录
+      this.judgeToken(myToken)
+    }
+  },
+  methods: {
+    judgeToken(myToken) {
+      console.log(myToken)
+      this.$axios
+        .post(
+          '/user/token',
+          {
+            token: myToken,
+          },
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          }
+        )
+        .then((res) => {
+          //传来的数据data为boolean，true表示未过期，否则已过期
+          if (res.data.data) {
+            //没有过期则继续使用
+            console.log('token未过期,可继续使用')
+          } else {
+            //已经过期则清除localStorage
+            console.log('token超时,已清除LocalStorage')
+            localStorage.clear()
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,45 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import axios from "axios";
+import router from "./router";
+import ElementUI from "element-ui";
+import store from "./store/index";
+import "element-ui/lib/theme-chalk/index.css";
 
-Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.config.productionTip = false;
+axios.defaults.baseURL = "http://localhost:8081";
+Vue.prototype.$axios = axios;
+const AUTH_TOKEN = localStorage.getItem("token");
+axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 2xx 范围内的状态码都会触发该函数。
+  //401自定义为token失效
+  // if(response.data.statusCode == '401'){
+  //   //token失效则只需要清空localStorage，对于token的判断交给router
+  //   alert("未登录！")
+  // }
+  return response;
+}, function (error) {
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么
+  
+  // if(error.response.status == 401){
+  //   alert("未登录！请重新登陆！")
+  //   router.push("/login")
+  //   // return error;
+  // }
+  return Promise.reject(error);
+});
+
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  //全局使用
+  render: (h) => h(App), //render函数中，渲染的是那个.vue组件，那么这个组件就是根组件
+  router,
+  axios,
+  store
+}).$mount("#app");
